13940fedf9106a0d73047ece46d1a75ee97f34c8
Hendra Sadewa <me@hendrasadewa.com>
2026-05-16 14:53:28 +0700

    fuck cors

--- src/middlewares/cors-middleware.ts
+++ src/middlewares/cors-middleware.ts
@@ -1,10 +1,5 @@
 import { cors } from "hono/cors";
 
 export function corsMiddleware() {
-  return cors({
-    allowHeaders: ["Content-Type", "Authorization", "User-Agent", "Origin"],
-    allowMethods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    maxAge: 600,
-    origin: ["http://localhost:5173", "https://pustaka.hendrasadewa.com"],
-  });
+  return cors();
 }
